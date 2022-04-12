c2aa754c9c06406bb074f177b1800ddefc02a520
Nic1493 <nic.choy107@gmail.com>
2022-04-12 12:56:01 -0400

    Project cleanup pt. 5
    - locked application target fps to 60
    - cleaned up player movement smoothing

--- Assets/Scripts/Game/GameInit.cs
+++ Assets/Scripts/Game/GameInit.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class GameInit : MonoBehaviour
 {
-    // enable visual effects at startup because they're causing frame drops in editor
     void Start()
     {
+        // enable visual effects at startup because they're causing frame drops in editor
         var effects = GetComponentsInChildren<MeshRenderer>(true);
         foreach (var effect in effects)
         {
             effect.enabled = true;
         }
+
+        // set application fps to 60
+        Application.targetFrameRate = 60;
     }
 }
